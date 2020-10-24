00c35f4df52e01b193b5282e85e01191cbc04b16
Yuri Matos <yuri.cabello@hotmail.com>
2020-10-24 02:59:57 -0300

    mobile ajustes

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-import { Container, Header } from './styles';
+import Tenis1 from '../../img/tenis1.jpeg';
+import Tenis2 from '../../img/tenis2.jpeg';
+import Tenis3 from '../../img/tenis3.jpeg';
+
+import { Container, Header, Content } from './styles';
 
 function home() {
   return (
@@ -9,9 +13,24 @@ function home() {
           <Header>
             <div className="oi"> 
               <h1>e-Virtual</h1>  
-              <button>oi</button>      
+              <a href="sobre"><i class="fas fa-shopping-cart"></i></a>
             </div> 
           </Header>
+          <Content>
+            <div className="card">
+              <img src={Tenis1} className="tenis1" alt="tenis1"/>
+              <p className="text">Tenis Nike</p>
+              <button className="but">Add</button>
+              <img src={Tenis2} className="tenis2" alt="tenis2"/>
+              <p className="text">Tenis Nike</p>
+              <button className="but">Add</button>
+              <img src={Tenis3} className="tenis3" alt="tenis3"/>
+              <p className="text">Tenis Nike</p>
+              <button className="but">Add</button>
+
+            
+            </div>
+          </Content>
         </Container>
 
       </>
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
 width: 100vw;
 height: 100vh;
-background: linear-gradient(700.54deg, #4f2727 0%, #000000 80%);
+background: linear-gradient(300.54deg, #000 10%, red 500%);
 `;
 
 export const Header = styled.div `
@@ -11,14 +11,63 @@ export const Header = styled.div `
     display: flex;
     justify-content: space-between;
   }
-  button{
-    width: 2.0rem;
-    height: 2.0rem;
+  .fas{
+    cursor: pointer;
+    color: #fff;
     margin: 1.0rem 1.0rem;
   }
   h1{
     color: #fff;
     margin: 1.0rem 1.0rem;
   }
-`;  
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  
+    .but{
+      width: 12rem;
+      height: 2.0rem;
+      padding-top: 0.1rem;
+      font-size: 1.5rem;
+      font-weight: bold;
+      margin-bottom: 2.0rem;
+    }
+    .text{
+      color: #fff;
+      display: flex;
+      justify-content: center;
+      padding-top: 0.5rem;
+      font-size: 1.5rem;
+    }
+  .tenis1{
+    width: 12.0rem;
+    display: flex;
+    margin-top: 3.0rem;
+  }
+
+  .tenis2{
+    width: 12.0rem;
+    display: flex;
+    margin-top: 3.0rem;
+
+  }
+
+  .tenis3{
+    width: 12.0rem;
+    display: flex;
+    margin-top: 3.0rem;
+
+  }
+
+  @media(min-width 700px;){
+    .tenis1{
+    width: 50px;
+    display: flex;
+    margin-top: 3.0rem;
+  } 
+
+
+`;
 
